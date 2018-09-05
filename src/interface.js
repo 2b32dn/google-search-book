42904d1da9339f6c59c4981348ec1453529e94dd
2b32dn <DicksonT_Lee@hotmail.com>
2018-09-05 14:55:25 -0400

    removedgooglekey

--- src/interface.js
+++ src/interface.js
@@ -13,7 +13,7 @@ class Interface extends Component{
     e.preventDefault()
     // console.log(e)
     // console.log(this.state.searchInput)
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.searchInput}&maxResults=40&key=AIzaSyAiVTzMNKM_dzWks7V4MiSchGeGih_u5ts`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.searchInput}&maxResults=40&key=(KEY)`)
       .then( res => res.json())
       .then( resJSON => this.setState({searchResults: resJSON.items}))
   }
